db5ee783236fd566074b46daa86c5b4586d430d2
Valeriya2511 <finsternis.ru@mail.ru>
2024-03-12 19:40:37 +0600

    feat: add solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -80,8 +80,11 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const str1 = JSON.stringify(obj1);
+  const str2 = JSON.stringify(obj2);
+  if (str1 === str2) return true;
+  return false;
 }
 
 /**
@@ -95,8 +98,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -115,8 +118,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -129,8 +132,19 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr = [];
+  const lengthWord = Object.values(lettersObject).flat().length;
+  for (let i = 0; i < lengthWord; i += 1) {
+    Object.entries(lettersObject).forEach(([elem, ind]) => {
+      ind.forEach((el) => {
+        if (el === i) {
+          arr.push(elem);
+        }
+      });
+    });
+  }
+  return arr.join('');
 }
 
 /**
